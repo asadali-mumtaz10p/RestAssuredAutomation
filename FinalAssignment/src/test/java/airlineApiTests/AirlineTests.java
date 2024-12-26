45171672849b9f4d3d916e76fbc02f6641cfdb2c
asad.mumtaz <asad.mumtaz@10pearls.com>
2024-12-26 09:35:45 +0500

    Updated Project

--- FinalAssignment/src/test/java/airlineApiTests/AirlineTests.java
+++ FinalAssignment/src/test/java/airlineApiTests/AirlineTests.java
@@ -11,30 +11,31 @@ import java.util.HashMap;
 public class AirlineTests extends PassengerAPIs {
 
     @Test
-    public void createPassenger(){
+    public void createPassenger() {
         String payload = Payloads.getPassengerPayloadViaString("asad", "5", "66038402-402d-4a3f-baef-7cb5f53697a8");
         Response response = createPassenger(payload);
-        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.statusCode(), 200);
     }
 
     @Test
-    public void getPassenger(){
-        String baseURL = "https://api.instantwebtools.net/v1/passenger/67651941596c781cbd71bce5";
+    public void getPassenger() {
+        String baseURL = "https://api.instantwebtools.net/v1/passenger/66a4ddc75736a93bef80c6be";
         Response response = RestAssured.get(baseURL);
-        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.statusCode(), 200);
     }
 
     @Test
-    public void updatePassenger(){
+    public void updatePassenger() {
         String payload = Payloads.updatePassengerPayloadViaString("asad ali");
         Response response = updatePassenger(payload);
-        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.statusCode(), 200);
     }
+
     @Test
-    public void deletePassenger(){
+    public void deletePassenger() {
         String baseURL = "https://api.instantwebtools.net/v1/passenger/67651941596c781cbd71bce5";
         Response response = RestAssured.delete(baseURL);
-        Assert.assertEquals(response.statusCode(),204);
+        Assert.assertEquals(response.statusCode(), 200);
     }
 
 }
